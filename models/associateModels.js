364bf94d0e46a63bad6481ffb15cd26578324f35
singhhokam <hokamsingh07@gmail.com>
2023-09-18 14:26:55 +0530

    updated associations

--- models/associateModels.js
+++ models/associateModels.js
@@ -5,13 +5,13 @@ const User = require("./user");
 
 function associate() {
   User.hasMany(Community, { foreignKey: "userId", as: "owner" });
-  Community.belongsTo(User, { foreignKey: "userId", as: "owner" });
+  Community.belongsTo(User);
 
   Community.hasMany(Member, { foreignKey: "communityId", as: "members" });
   Member.belongsTo(Community);
 
   User.hasMany(Member, { foreignKey: "userId", as: "user" });
-  Member.belongsTo(User, { foreignKey: "userId", as: "user" });
+  Member.belongsTo(User);
 
   Member.belongsTo(Role, { foreignKey: "roleId", as: "role" });
 }
